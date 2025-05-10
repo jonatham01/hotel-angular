5a7b9aba18ff3699ae63153c3f80116e71953239
jonathan Diaz <jonatham01@hotmail.com>
2025-05-09 19:02:32 -0500

    attribute service

--- src/app/services/attribute-room.service.ts
+++ src/app/services/attribute-room.service.ts
@@ -1,9 +1,80 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { RoomAttributeRequestDTO, RoomAttributeResponseDTO } from '../models/roomAttribute.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttributeRoomService {
 
-  constructor() { }
+  apiUrl:string = "/fees";
+    
+    private handleError = (error: HttpErrorResponse) => {
+      return throwError(() => new Error('Error' + error.message));
+    };
+  
+  constructor(
+      private httpClient:HttpClient
+    ) { }
+
+  create(dto: RoomAttributeRequestDTO, file: File): Observable<RoomAttributeResponseDTO> {
+    const formData = new FormData();
+    // Convertir el objeto DTO a JSON y empaquetarlo como un Blob
+    const jsonBlob = new Blob([JSON.stringify(dto)], { type: 'application/json' });
+
+    formData.append('data', jsonBlob); // Parte llamada "data" (JSON)
+    formData.append('file', file);     // Parte llamada "file" (archivo)
+
+    return this.httpClient.post<RoomAttributeResponseDTO>(
+      `${this.apiUrl}/create`,
+      formData
+  ).pipe(
+    catchError(this.handleError)
+  );
+}
+  
+    getHotel(id: number): Observable<RoomAttributeResponseDTO> {
+      const params = new HttpParams().set('id', id);
+      return this.httpClient.get<RoomAttributeResponseDTO>(
+        `${this.apiUrl}/id`, {params}
+      ).pipe(
+        catchError(this.handleError)
+      );
+    }
+  
+    getAll(): Observable<RoomAttributeResponseDTO[]> {
+      return this.httpClient.get<RoomAttributeResponseDTO[]>(
+        `${this.apiUrl}`
+      ).pipe(
+        catchError(this.handleError)
+      );
+    }
+
+    update(id: number, dto: RoomAttributeRequestDTO, file: File): Observable<RoomAttributeResponseDTO> {
+      const formData = new FormData();
+
+      // Convertir el DTO en un Blob JSON
+      const jsonBlob = new Blob([JSON.stringify(dto)], { type: 'application/json' });
+
+      formData.append('data', jsonBlob); // parte "data"
+      formData.append('file', file);     // parte "file"
+
+      return this.httpClient.put<RoomAttributeResponseDTO>(
+        `${this.apiUrl}/${id}`,
+        formData
+      ).pipe(
+        catchError(this.handleError)
+      );
+    }
+
+  
+    delete(id: bigint) {
+      this.httpClient.delete<void>(
+        `${this.apiUrl}/delete/${id}`
+      ).pipe(
+        catchError(this.handleError)
+      );
+    }
+    
 }
--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ReservationRequestDTO, ReservationResponseDTO } from '../models/reservation.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  apiUrl:string = "//api/payment-transactions";
+        
+  private handleError = (error: HttpErrorResponse) => {
+    return throwError(() => new Error('Error' + error.message));
+  };
+      
+  constructor( private httpClient:HttpClient) { }
+    
+      create(dto:ReservationRequestDTO): Observable<ReservationResponseDTO> {
+          return this.httpClient.post<ReservationResponseDTO>(
+            `${this.apiUrl}`, dto
+          ).pipe(
+            catchError(this.handleError)
+          );
+        }
+      
+        getOne(id: bigint): Observable<ReservationResponseDTO> {
+          return this.httpClient.get<ReservationResponseDTO>(
+            `${this.apiUrl}/id/${id}`
+          ).pipe(
+            catchError(this.handleError)
+          );
+        }
+      
+        getAll(): Observable<ReservationResponseDTO[]> {
+          return this.httpClient.get<ReservationResponseDTO[]>(
+            `${this.apiUrl}/all`
+          ).pipe(
+            catchError(this.handleError)
+          );
+        }
+    
+        update(id: bigint, dto:ReservationRequestDTO): Observable<ReservationResponseDTO> {
+          return this.httpClient.put<ReservationResponseDTO>(
+            `${this.apiUrl}/update/${id}`, dto
+          ).pipe(
+            catchError(this.handleError)
+          );
+        }
+      
+        delete(id: bigint) {
+          this.httpClient.delete<void>(
+            `${this.apiUrl}/delete/${id}`
+          ).pipe(
+            catchError(this.handleError)
+          );
+        }
 }
