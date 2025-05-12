1651a3664fb0ad02b141720f06922398137c739c
jonathan Diaz <jonatham01@hotmail.com>
2025-05-12 06:47:08 -0500

    admin routing part 1

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from '../interceptors/token.interceptor';
 
+import { AdminRoutingModule } from './admin-routing.module';
+import { UserComponent } from './user/user.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
+    UserComponent,
+    ReservationsComponent,
+    RoomsComponent
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  imports: [
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './admin/user/user.component';
+import { ReservationsComponent } from './admin/reservations/reservations.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/admin/user',
+    pathMatch:'full'
+  },
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'reservations',
+    component:ReservationsComponent
+  },
+  {
+    path:'rooms',
+    component:RoomsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
