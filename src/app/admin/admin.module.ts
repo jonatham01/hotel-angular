3ae0770db561c1d76c637dc01ab95f254e8bfbf4
jonathan Diaz <jonatham01@hotmail.com>
2025-05-12 06:50:06 -0500

    TokenInterceptor at admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,8 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { UserComponent } from './user/user.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from '../interceptors/token.interceptor';
 
 
 @NgModule({
@@ -16,6 +18,13 @@ import { RoomsComponent } from './rooms/rooms.component';
   imports: [
     CommonModule,
     AdminRoutingModule
+  ],
+  providers:[
+    {
+      provide : HTTP_INTERCEPTORS, 
+      useClass: TokenInterceptor, 
+      multi: true
+    }
   ]
 })
 export class AdminModule { }
